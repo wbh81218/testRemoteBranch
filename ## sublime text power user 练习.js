d4151670521195de1457e43ea27998737fb69e40
wangbinhui <binhui.wang81218@gmail.com>
2019-08-07 21:49:39 +0800

    测试sublime git commit

---
+++ ## sublime text power user 练习.js
@@ -0,0 +1,28 @@
+var wes = {jjj};
+wes.firstName = "wesley";
+wes.lastName = "bos";
+wes.status = "Cool guy";
+if (!!wes.status.match(/cool/gi)) { wes.score = 100; } else { wes.score = 10; }
+
+        console.log([]);
+        
+        var len = myArr.length;
+        for(var i=0; i<len;i++){
+            console.log(myArr[i]);
+        }
+                var len = .length;
+                for(var =0; <len;++){
+                }
+wes
+wes
+kaitlin
+wes
+awesome
+western
+wes
+<ul>
+	<li> go to the store </li>
+	<li> buy some apples </li>
+	<li> peel those aooles </li>
+	<li> bake me a pie </li>
+</ul>
---
+++ test.js
@@ -0,0 +1,23 @@
+var obj = {
+    mystr: "hello",
+    myFunc: function(){
+        console.log("hello");
+        $('.smeDiv').animate({hegiht:500},function(){
+            //trigger the callback
+            console.log("Done");
+        });
+    },
+    myFunc2: function(){
+        return "Just another level 2 function";
+    },
+    myNum: function(){
+        return "Level 2 function";
+    }
+}
+
+var somefn = function(){
+    
+},,,,,,
+var somefn = function(){
+
+};;;;;;;;
