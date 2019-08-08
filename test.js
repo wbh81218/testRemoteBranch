7cc53b30683cfe65f907bea924e34936311e000c
wangbinhui <binhui.wang81218@gmail.com>
2019-08-08 12:55:43 +0800

    test sublimerge

--- test.js
+++ test.js
@@ -1,20 +1,2 @@
-var obj = {
-    mystr: "hello",
-    myFunc: function(){
-        console.log("hello");
-        $('.smeDiv').animate({hegiht:500},function(){
-            //trigger the callback
-            console.log("Done");
-        });
-    },
-    myFunc2: function(){
-        return "Just another level 2 function";
-    },
-    myNum: function(){
-        return "Level 2 function";
-    }
-}
-
-var somefn = function(){
-     var test = 1;
-}
+var test =2;
+tet;
